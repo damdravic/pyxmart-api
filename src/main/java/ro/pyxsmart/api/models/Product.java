68cc91ed592652781d14264774dc2fb5ec010528
Dragos <damian.dragosv@gmail.com>
2026-08-22 11:26:21 +0300

    Feat: product

---
+++ src/main/java/ro/pyxsmart/api/models/Product.java
@@ -0,0 +1,13 @@
+package ro.pyxsmart.api.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+     private Long id;
+     private String name;
+     private String code;
+     private String description;
+     private double price;
+
+}
